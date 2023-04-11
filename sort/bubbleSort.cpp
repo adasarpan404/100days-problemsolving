93c5e9f56f3b0da01a2a12070a442cb8278b272c
Arpan Das <dasarpan471@gmail.com>
2023-04-11 20:52:48 +0530

    sorting started

---
+++ sort/bubbleSort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[] = {4, 5, 1, 2, 3};
+    int n = sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
---
+++ sort/selectionSort.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[] = {4, 5, 1, 2, 3};
+    int n = sizeof(a)/sizeof(a[0]);
+
+    for (int i = 0; i < n-1; i++)
+    {
+
+        int min_idx = i;
+        for (int j = i+1; j < n; j++)
+        {
+          if (a[j] < a[min_idx])
+              min_idx = j;
+        }
+
+        if (min_idx!=i)
+            swap(a[min_idx], a[i]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
